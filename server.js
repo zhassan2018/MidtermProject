c951aa0761f592015205a47310456be940a5124e
Zohaib <zohaib.hassan@mail.mcgill.ca>
2018-06-28 19:42:05 +0000

    Fixed form request

--- server.js
+++ server.js
@@ -59,10 +59,108 @@ app.get("/", (req, res) => {
 app.post("/userInput", (req, res) =>{
   var uRequest = req.body['userData'];
   toShow['first'] = uRequest
-var url = `http://www.omdbapi.com/?apikey=4cf6233d&t=${uRequest}`
 
 
- res.render("index", toShow)
+  // EBAY API
+
+  var params = {
+    keywords: [uRequest],
+    outputSelector: ['AspectHistogram'],
+    paginationInput: {
+      entriesPerPage: 2
+    },
+
+    itemFilter: [
+      {name: 'FreeShippingOnly', value: true},
+      {name: 'MaxPrice', value: '150'}
+    ],
+
+    itemFilter: [
+      {name: 'FreeShippingOnly', value: true},
+      {name: 'MaxPrice', value: '150'}
+    ],
+
+    domainFilter: [
+      {name: 'domainName', value: 'Digital_Cameras'}
+    ]
+  };
+
+  ebay.xmlRequest({
+      serviceName: 'Finding',
+      opType: 'findItemsByKeywords',
+      appId: process.env.THIERRY_EBAY_KEY,
+      params: params,
+      parser: ebay.parseResponseJson    // (default)
+    },
+    // gets all the items together in a merged array
+    function itemsCallback(error, itemsResponse) {
+      if (error) throw console.log(error);
+
+
+      var items = itemsResponse.searchResult.item;
+
+      if (items === undefined){
+        return console.log("Could not find item:",params.keywords,"on EBAY");
+
+      }
+
+
+
+
+      for (var i = 0; i < items.length; i++) {
+        console.log('- ' + items[i].title, "PRODUCT");
+      }
+    }
+  );
+
+
+  //YELP API
+
+  const client = yelp.client(process.env.YELP_API_KEY);
+  var food = uRequest
+
+  client.search({
+    term:food,
+    location: 'montreal, qc',
+    sort_by: 'rating',
+    limit: 2,
+    price: [2,1]
+  }).then(response => {
+    if(response.statusCode !== 200){
+      return console.log("Didnt find a restaurant for", food )
+    }
+
+  if(response.jsonBody.businesses.length === 0){
+    return console.log("Didnt find a restaurant for", food )
+  }
+    for(var i = 0; i<response.jsonBody.businesses.length;i++){
+      console.log(response.jsonBody.businesses[i].name,"-Restaurant" );
+    }
+  })
+
+
+  //Movies/TV API
+
+
+  fetch(`http://www.omdbapi.com/?apikey=${process.env.MOVIES_API}&t=${uRequest}`)
+      .then(res => res.json())
+      .then((json) => {
+        if (json.Response === 'False'){
+          return console.log(json.Error)
+        }
+        console.log(json.Title, "-MOVIE")})
+
+
+  fetch( `https://www.googleapis.com/books/v1/volumes?q=${uRequest}&maxResults=1&projection=lite&key=${process.env.BOOKS_API}`).then(res => res.json()).then((json) => {
+        if (json.totalItems === 0){
+          return console.log('book not found!')
+        }
+
+        console.log(json.items[0].volumeInfo.title, "-BOOK")
+      })
+
+
+
   res.redirect('/')
 
 })
@@ -74,105 +172,6 @@ app.listen(PORT, () => {
 
 
 
-// EBAY API
-
-var params = {
-  keywords: ["fjdkslfjdsklfjdl"],
-  outputSelector: ['AspectHistogram'],
-  paginationInput: {
-    entriesPerPage: 10
-  },
-
-  itemFilter: [
-    {name: 'FreeShippingOnly', value: true},
-    {name: 'MaxPrice', value: '150'}
-  ],
-
-  itemFilter: [
-    {name: 'FreeShippingOnly', value: true},
-    {name: 'MaxPrice', value: '150'}
-  ],
-
-  domainFilter: [
-    {name: 'domainName', value: 'Digital_Cameras'}
-  ]
-};
-
-console.log(process.env.THIERRY_EBAY_KEY)
-ebay.xmlRequest({
-    serviceName: 'Finding',
-    opType: 'findItemsByKeywords',
-    appId: process.env.THIERRY_EBAY_KEY,
-    params: params,
-    parser: ebay.parseResponseJson    // (default)
-  },
-  // gets all the items together in a merged array
-  function itemsCallback(error, itemsResponse) {
-    if (error) throw console.log(error);
-
-
-    var items = itemsResponse.searchResult.item;
-
-    if (items === undefined){
-      return console.log("Could not find item:",params.keywords,"on EBAY");
-
-    }
-
-
-
-    console.log('Found', items.length, 'items');
-
-    for (var i = 0; i < items.length; i++) {
-      console.log('- ' + items[i].title);
-    }
-  }
-);
-
-
-//YELP API
-
-const client = yelp.client(process.env.YELP_API_KEY);
-var food = "hamburger"
-
-client.search({
-  term:food,
-  location: 'montreal, qc',
-  sort_by: 'rating',
-  limit: 20,
-  price: [2,1]
-}).then(response => {
-  if(response.statusCode !== 200){
-    return console.log("Didnt find a restaurant for", food )
-  }
-
-if(response.jsonBody.businesses.length === 0){
-  return console.log("Didnt find a restaurant for", food )
-}
-  for(var i = 0; i<response.jsonBody.businesses.length;i++){
-    console.log(response.jsonBody.businesses[i].name);
-  }
-})
-
-
-//Movies/TV API
-
-
-fetch(`http://www.omdbapi.com/?apikey=${process.env.MOVIES_API}&t=jfldkjfld`)
-    .then(res => res.json())
-    .then((json) => {
-      if (json.Response === 'False'){
-        return console.log(json.Error)
-      }
-      console.log(json)})
-
-
-fetch( `https://www.googleapis.com/books/v1/volumes?q=dsfkjsdkfljd&maxResults=1&projection=lite&key=${process.env.BOOKS_API}`).then(res => res.json()).then((json) => {
-      if (json.totalItems === 0){
-        return console.log('book not found!')
-      }
-
-      console.log(json.items[0].volumeInfo.title)
-    })
 
 
 
