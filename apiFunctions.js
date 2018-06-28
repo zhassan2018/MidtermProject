3b4e3387c2ad2202b9cc1585a2a2c4e7fdaed45f
Zohaib <zohaib.hassan@mail.mcgill.ca>
2018-06-28 20:42:26 +0000

    All API functions added

--- apiFunctions.js
+++ apiFunctions.js
@@ -12,7 +12,7 @@ const ENV = process.env.ENV || "development";
 
 uRequest="harry potter"
 
-var MovieAPi = (uRequest) => {
+var MovieAPI = (uRequest) => {
   fetch(`http://www.omdbapi.com/?apikey=${process.env.MOVIES_API}&t=${uRequest}`)
       .then(res => res.json())
       .then((json) => {
@@ -22,5 +22,93 @@ var MovieAPi = (uRequest) => {
         console.log(json.Title, "-MOVIE")})
 }
 
-MovieAPi(uRequest)
+var BookAPI = (uRequest) => {
+  fetch( `https://www.googleapis.com/books/v1/volumes?q=${uRequest}&maxResults=1&projection=lite&key=${process.env.BOOKS_API}`).then(res => res.json()).then((json) => {
+        if (json.totalItems === 0){
+          return console.log('book not found!')
+        }
+
+        console.log(json.items[0].volumeInfo.title, "-BOOK")
+      })
+
+}
+
+var yelpAPI = (uRequest) => {
+  const client = yelp.client(process.env.YELP_API_KEY);
+  var food = uRequest
+
+  client.search({
+    term:food,
+    location: 'montreal, qc',
+    sort_by: 'rating',
+    limit: 2,
+    price: [2,1]
+  }).then(response => {
+    if(response.statusCode !== 200){
+      return console.log("Didnt find a restaurant for", food )
+    }
+
+  if(response.jsonBody.businesses.length === 0){
+    return console.log("Didnt find a restaurant for", food )
+  }
+    for(var i = 0; i<response.jsonBody.businesses.length;i++){
+      console.log(response.jsonBody.businesses[i].name,"-Restaurant" );
+    }
+  })
+}
+
+var ebayAPI = (uRequest) => {
+  var params = {
+    keywords: [uRequest],
+    outputSelector: ['AspectHistogram'],
+    paginationInput: {
+      entriesPerPage: 2
+    },
+
+    itemFilter: [
+      {name: 'FreeShippingOnly', value: true},
+      {name: 'MaxPrice', value: '150'}
+    ],
+
+    itemFilter: [
+      {name: 'FreeShippingOnly', value: true},
+      {name: 'MaxPrice', value: '150'}
+    ],
+
+    domainFilter: [
+      {name: 'domainName', value: 'Digital_Cameras'}
+    ]
+  };
+
+  ebay.xmlRequest({
+      serviceName: 'Finding',
+      opType: 'findItemsByKeywords',
+      appId: process.env.THIERRY_EBAY_KEY,
+      params: params,
+      parser: ebay.parseResponseJson    // (default)
+    },
+    // gets all the items together in a merged array
+    function itemsCallback(error, itemsResponse) {
+      if (error) throw console.log(error);
+
+
+      var items = itemsResponse.searchResult.item;
+
+      if (items === undefined){
+        return console.log("Could not find item:",params.keywords,"on EBAY");
+
+      }
+
+
+
+
+      for (var i = 0; i < items.length; i++) {
+        console.log('- ' + items[i].title, "PRODUCT");
+      }
+    }
+  );
+}
+
+
+
 
